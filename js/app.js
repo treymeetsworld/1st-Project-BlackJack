3aa932b0e1cec703c5587ed684ecb4ee32133072
Trevor Mwangi <trevorzac16@yahoo.com>
2021-10-18 10:27:45 -0400

    define const's and variables

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/*----------------- Constants -----------------*/
+const suits = ["♥️","♦️","♠️ ","♣️"]
+const cardValues =[2,3,4,5,6,7,8,9,10,"J" = 10,"Q" =10,"K" =10,"A" = [1,11] ]
+/*------------- Variables (state) -------------*/
+let deck,hit,stand,turn,winner
+/*--------- Cached Element References ---------*/
+
+/*-------------- Event Listeners --------------*/
+
+/*----------------- Functions -----------------*/
