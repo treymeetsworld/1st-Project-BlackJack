68b2516ccedc9a27789b9cf09b8d0bd799379627
Trevor Mwangi <trevorzac16@yahoo.com>
2021-10-18 12:00:06 -0400

    Add deck function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*----------------- Constants -----------------*/
 const suits = ["♥️","♦️","♠️ ","♣️"]
 const cards =["2","3","4","5",
- "6","7","8","9",
+"6","7","8","9",
   "10","J","Q","K","A"]
 /*------------- Variables (state) -------------*/
 let deck,hit,stand,turn,winner
@@ -9,11 +9,32 @@ let deck,hit,stand,turn,winner
 let standRef = document.getElementById('stand')
 let hitRef = document.getElementById('hit') 
 /*-------------- Event Listeners --------------*/
-standRef.addEventListener('click',)
-hitClick = hitRef.addEventListener('click',)
+standRef.addEventListener('click',console.log('hi'))
+hitClick = hitRef.addEventListener('click',console.log('hello'))
 /*----------------- Functions -----------------*/
 init()
 
 function init(){
+deck =getDeck()
+turn = 1
+winner = null
+}
+// render()
+// }
+// function handleclick(){
 
+// }
+
+function getDeck(){
+  let deck = []
+  for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    for (let cardsIdx = 0; cardsIdx < cards.length; cardsIdx++) {
+      let card = {
+        suit: suits[suitIdx],
+        cards: cards[cardsIdx]
+      }
+      deck.push(card);
+    }
+  }
+  console.log(deck);
 }
