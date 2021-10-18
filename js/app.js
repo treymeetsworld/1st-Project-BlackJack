37504e1a2232f016401ae0a61a8beafcab2d637a
Trevor Mwangi <trevorzac16@yahoo.com>
2021-10-18 11:03:34 -0400

    Add event listeners

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 /*----------------- Constants -----------------*/
 const suits = ["♥️","♦️","♠️ ","♣️"]
-const cardValues =[2,3,4,5,6,7,8,9,10,"J" = 10,"Q" =10,"K" =10,"A" = [1,11] ]
+const cards =["2","3","4","5",
+ "6","7","8","9",
+  "10","J","Q","K","A"]
 /*------------- Variables (state) -------------*/
 let deck,hit,stand,turn,winner
 /*--------- Cached Element References ---------*/
-
+let standRef = document.getElementById('stand')
+let hitRef = document.getElementById('hit') 
 /*-------------- Event Listeners --------------*/
-
+standRef.addEventListener('click',)
+hitClick = hitRef.addEventListener('click',)
 /*----------------- Functions -----------------*/
+init()
+
+function init(){
+
+}
