63d640b4261aec527ab00db7c0e28334d0bc8b37
Trevor Mwangi <trevorzac16@yahoo.com>
2021-10-21 00:36:08 -0400

    Fix hit function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const suits = ["♥️","♦️","♠️","♣️"]
 const cards =["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
 
 /*------------- Variables (state) -------------*/
-let stand,turn,winner,playerCard1, playerCard2, dealerCard1, dealerCard2 
+let turn,winner,playerCard1, playerCard2, dealerCard1, dealerCard2 
 let deck = []
 let cardsOut = []
 let playerTotal = []
@@ -24,9 +24,9 @@ let dealInputEl = document.getElementById('dealerInput')
 let playInputEl = document.getElementById('playerInput')
 
 /*-------------- Event Listeners --------------*/
-// standRef.addEventListener('click',console.log('hi'))
+standRef.addEventListener('click',stand)
 hitClick = hitRef.addEventListener('click',hit)
-// dealRef.addEventListener('click',console.log('hi'))
+dealRef.addEventListener('click',deal)
 // startRef.addEventListener('click',init)
 /*----------------- Functions -----------------*/
 init()
@@ -36,14 +36,6 @@ function init(){
   shuffleDeck(deck)
   turn = 1
   winner = null
-  p1El.innerText = cardsOut.pop()
-  p2El.innerText = cardsOut.pop()
-
-  d1El.innerText = cardsOut.pop()
-  d2El.innerText = cardsOut.pop()
-  deckEl.innerText = cardsOut.length
-  
-  cardValues()
 }
 
 
@@ -71,6 +63,13 @@ function shuffleDeck(deck){
   }
   return cardsOut
 }
+function deal(){
+  p1El.innerText = cardsOut.pop()
+  p2El.innerText = cardsOut.pop()
+  d1El.innerText = cardsOut.pop()
+  deckEl.innerText = cardsOut.length
+  cardValues()
+}
 
 function cardValues() {
   playerCard1 = parseInt(p1El.innerText.replace(/[♥️♦️♠️♣️]/,'').replace(/[KQJ]/,'10').replace(/[A]/,'11'))
@@ -90,16 +89,23 @@ function cardValues() {
 }
 
 function hit(){ 
+  let sum 
   p1El.innerText = cardsOut.pop()
+  deckEl.innerText = cardsOut.length
   playerHit = p1El.innerText
     playerHit =  parseInt(p1El.innerText.replace(/[♥️♦️♠️♣️]/,'').replace(/[KQJ]/,'10').replace(/[A]/,'11'))
     playerTotal.push(playerHit)
-    let sum = playerTotal.reduce((a,b)=>{
+    sum = playerTotal.reduce((a,b)=>{
       return a + b
     },0)
-    playInputEl.value = sum
+    playInputEl.value = parseInt(sum)
+    playerTotal = parseInt(playInputEl.value)
   }
 
 
-
+function stand(){
+  d2El.innerText = cardsOut.pop()
+  deckEl.innerText = cardsOut.length
+  cardValues()
+}
 
